8be3d63a224795a385742ea8e69e625c858e4f13
Manish Mohan <manish.mohan@BLDM3217-MAC.local>
2019-10-14 12:35:14 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table
+      :employees="employees"
+      :columns="columns"
+    />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/Table.vue";
+
+export default {
+  name: "app",
+  components: {
+  EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Henricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ],
+      columns: ["id","name","email"]
+    };
+  },
+
+};
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+button:hover,
+button:active,
+button:focus {
+  background: #32a95d;
+  border: 1px solid #32a95d;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+    <table class="table">
+      <thead>
+          <tr>
+              <th v-for="(column, index) in columns" :key="index"> {{column}}</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="(item, index) in employees" :key="index">
+              <td v-for="(column, indexColumn) in columns" :key="indexColumn">{{item[column]}}</td>
+          </tr>
+      </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+  name: "employee-table",
+  props: {
+    employees: Array,
+    columns: Array
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin: 0 0.5rem 0 0;
+}
+
+input {
+  margin: 0;
+}
+
+.empty-table {
+  text-align: center;
+}
+</style>
